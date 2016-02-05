39d4d3c9e14d87397ca118cc6889f6d2b19c3cc3
Rich Jones <rich@anomos.info>
2016-02-05 20:48:55 +0100

    0.10.2 - Lambda is 2.7 only, and it makes the tests fail, so change the supported versions and test targets

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 
 setup(
     name='zappa',
-    version='0.10.1',
+    version='0.10.2',
     packages=['zappa'],
     install_requires=required,  
     include_package_data=True,
@@ -29,11 +29,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
