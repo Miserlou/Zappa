bb11af1819e0fa98a269e0c1b7b79d3c21e68457
Dmitry Smal <mialinx@gmail.com>
2016-04-23 14:44:24 +0300

    added dynamodb access to default policy

--- zappa/zappa.py
+++ zappa/zappa.py
@@ -30,22 +30,22 @@ TEMPLATE_MAPPING = """{
   "headers": {
     #foreach($header in $input.params().header.keySet())
     "$header": "$util.escapeJavaScript($input.params().header.get($header))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "method": "$context.httpMethod",
   "params": {
     #foreach($param in $input.params().path.keySet())
     "$param": "$util.escapeJavaScript($input.params().path.get($param))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "query": {
     #foreach($queryParam in $input.params().querystring.keySet())
     "$queryParam": "$util.escapeJavaScript($input.params().querystring.get($queryParam))" #if($foreach.hasNext),#end
-    
+
     #end
-  }  
+  }
 }"""
 
 POST_TEMPLATE_MAPPING = """#set($rawPostData = $input.path('$'))
@@ -54,22 +54,22 @@ POST_TEMPLATE_MAPPING = """#set($rawPostData = $input.path('$'))
   "headers": {
     #foreach($header in $input.params().header.keySet())
     "$header": "$util.escapeJavaScript($input.params().header.get($header))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "method": "$context.httpMethod",
   "params": {
     #foreach($param in $input.params().path.keySet())
     "$param": "$util.escapeJavaScript($input.params().path.get($param))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "query": {
     #foreach($queryParam in $input.params().querystring.keySet())
     "$queryParam": "$util.escapeJavaScript($input.params().querystring.get($queryParam))" #if($foreach.hasNext),#end
-    
+
     #end
-  }  
+  }
 }"""
 
 FORM_ENCODED_TEMPLATE_MAPPING = """
@@ -78,22 +78,22 @@ FORM_ENCODED_TEMPLATE_MAPPING = """
   "headers": {
     #foreach($header in $input.params().header.keySet())
     "$header": "$util.escapeJavaScript($input.params().header.get($header))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "method": "$context.httpMethod",
   "params": {
     #foreach($param in $input.params().path.keySet())
     "$param": "$util.escapeJavaScript($input.params().path.get($param))" #if($foreach.hasNext),#end
-    
+
     #end
   },
   "query": {
     #foreach($queryParam in $input.params().querystring.keySet())
     "$queryParam": "$util.escapeJavaScript($input.params().querystring.get($queryParam))" #if($foreach.hasNext),#end
-    
+
     #end
-  }  
+  }
 }"""
 
 ASSUME_POLICY = """{
@@ -152,6 +152,13 @@ ATTACH_POLICY = """{
                 "sqs:*"
             ],
             "Resource": "arn:aws:sqs:::*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:*"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:*"
         }
     ]
 }"""
@@ -609,7 +616,7 @@ class Zappa(object):
             post_template_mapping = POST_TEMPLATE_MAPPING
             form_encoded_template_mapping = FORM_ENCODED_TEMPLATE_MAPPING
             content_mapping_templates = {
-                'application/json': template_mapping, 
+                'application/json': template_mapping,
                 'application/x-www-form-urlencoded': post_template_mapping,
                 'multipart/form-data': form_encoded_template_mapping
             }
@@ -852,7 +859,7 @@ class Zappa(object):
 
         # Automatically load credentials from config or environment
         if not boto_session:
-            
+
             # Set aws_region to None to use the system's region instead
             if self.aws_region is None:
                 self.aws_region = boto3.Session().region_name
