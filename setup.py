9eac4282b838cf0633cc4aa385450149560ed62a
Rich Jones <rich@anomos.info>
2016-04-26 15:14:00 -0400

    0.17.6 - Adds a workaround for non-US streaming upload failures

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f
 
 setup(
     name='zappa',
-    version='0.17.5',
+    version='0.17.6',
     packages=['zappa'],
     install_requires=required,
     tests_require=test_required,
