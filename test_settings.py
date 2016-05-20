944a724235515423eb56ccc8ded3f91163b38315
Rich Jones <rich@anomos.info>
2016-05-20 14:09:57 -0400

    update test settings

--- test_settings.py
+++ test_settings.py
@@ -3,6 +3,8 @@ APP_FUNCTION = 'hello_world'
 DEBUG = 'True'
 LOG_LEVEL = 'DEBUG'
 SCRIPT_NAME = 'hello_world'
+DOMAIN = None
+API_STAGE = 'ttt555'
 
 def prebuild_me():
     print("This is a prebuild script!")
