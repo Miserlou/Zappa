ddbc945c9d77fb4b1dacaa8600f6fac381d077de
Rich Jones <rich@anomos.info>
2016-08-04 14:15:37 -0700

    0.20.2 - remove [security] dep from requests

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f
 
 setup(
     name='zappa',
-    version='0.20.1',
+    version='0.20.2',
     packages=['zappa'],
     install_requires=required,
     tests_require=test_required,
