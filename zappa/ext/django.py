bc61becf1ce088e22bd1eff8456cda34257fdc3e
Rich Jones <rich@anomos.info>
2016-08-31 13:11:09 -0400

    revert, fix django setup error

--- zappa/ext/django.py
+++ zappa/ext/django.py
@@ -1,17 +1,13 @@
 import sys
-import os
 
 # add the Lambda root path into the sys.path
 sys.path.append('/var/task')
 
-# We're not going to require importing all of Django just to test.
-try: # pragma: no cover
-    from django.core.handlers.wsgi import WSGIHandler
-    from django.core.wsgi import get_wsgi_application
-except ImportError as e:
-    pass
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
+import os
 
-def get_django_wsgi(settings_module): # pragma: no cover
+def get_django_wsgi(settings_module):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
     import django
