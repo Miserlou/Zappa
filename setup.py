758bffbade357818bc8709097b97ba3edba9913f
Rich Jones <rich@anomos.info>
2016-09-02 13:21:26 -0400

    0.22.4 - remove expired policies upon update, fix lingering keep warms in the aws console

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f
 
 setup(
     name='zappa',
-    version='0.22.3',
+    version='0.22.4',
     packages=['zappa'],
     install_requires=required,
     tests_require=test_required,
