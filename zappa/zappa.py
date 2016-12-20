5cce67460ea84db22d9b0471d7cf2a24a9dfaf43
Paul Jimenez <pj@place.org>
2016-12-20 10:04:36 -0500

    Fix the returned order of events in logs

--- zappa/zappa.py
+++ zappa/zappa.py
@@ -1818,14 +1818,19 @@ class Zappa(object):
 
         all_streams = streams['logStreams']
         all_names = [stream['logStreamName'] for stream in all_streams]
-        response = self.logs_client.filter_log_events(
-            logGroupName=log_name,
-            logStreamNames=all_names,
-            filterPattern=filter_pattern,
-            limit=limit
-        )
+        fle_args = {'logGroupName':log_name,
+                    'logStreamNames':all_names,
+                    'filterPattern':filter_pattern,
+                    'limit':limit
+                    }
+        events = []
+        response = {}
+        while not response or 'nextToken' in response:
+            if 'nextToken' in response: fle_args['nextToken'] = response['nextToken']
+            response = self.logs_client.filter_log_events(**fle_args)
+            events += response['events']
 
-        return response['events']
+        return sorted(events, key=lambda k: k['timestamp'])
 
     def remove_log_group(self, group_name):
         """
