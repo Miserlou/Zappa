309417c8fbb7ddbb96cdfd27a980af65ac7357da
Rich Jones <rich@anomos.info>
2017-01-07 21:20:45 +0100

    include distinfo regardless

--- zappa/zappa.py
+++ zappa/zappa.py
@@ -194,9 +194,12 @@ API_GATEWAY_REGIONS = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-central-1', 'e
 LAMBDA_REGIONS = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-central-1', 'eu-west-1', 'ap-northeast-1',
                   'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2']
 
+# We never need to include these.
+# Related: https://github.com/Miserlou/Zappa/pull/56
+# Related: https://github.com/Miserlou/Zappa/pull/581
 ZIP_EXCLUDES = [
     '*.exe', '*.DS_Store', '*.Python', '*.git', '.git/*', '*.zip', '*.tar.gz',
-    '*.hg', '*.egg-info', 'pip', 'docutils*', 'setuputils*', '*.dist-info',
+    '*.hg', '*.egg-info', 'pip', 'docutils*', 'setuputils*'
 ]
 
 ##
