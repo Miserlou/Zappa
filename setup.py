ea9fa10c946d8f725e248501d6de6fbf65fb8f8d
longzhi <longzhi@gmail.com>
2017-04-25 17:18:29 +0800

    Clean code

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f
 
 setup(
     name='zappa',
-    version='0.32.1.dev',
+    version='0.32.1',
     packages=['zappa'],
     install_requires=required,
     tests_require=test_required,
