75a3aa0cd9972ef91e20ce4d1fb1bdbee687cecc
Rich Jones <rich@anomos.info>
2017-05-02 11:21:59 -0400

    add 3.6 to setuppy

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
