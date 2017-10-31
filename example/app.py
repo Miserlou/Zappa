a2d7e360df6250130ae9285c04d0bbf06c05481e
Miguel Ángel Campos <miammiel@hotmail.com>
2017-10-30 21:49:47 -0600

    Added Angular 4 example

--- example/app.py
+++ example/app.py
@@ -1,16 +1,19 @@
 import logging
-from flask import Flask
+from flask import Flask, current_app
 
 app = Flask(__name__)
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def lambda_handler(event=None, context=None):
-    logger.info('Lambda function invoked index()')
+    return current_app.send_static_file('index.html')
 
-    return 'hello from Flask!'
+@app.route('/static/<string:page_name>', methods=['GET'])
+def render_static(page_name):
+    return current_app.send_static_file(page_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
