9760cdba4c6c20899680ea34cadc6be5d6043d1d
João Miguel Neves <github@silvaneves.org>
2018-02-07 21:45:39 +0000

    Missed one

--- zappa/core.py
+++ zappa/core.py
@@ -171,7 +171,7 @@ ATTACH_POLICY = """{
 API_GATEWAY_REGIONS = ['us-east-1', 'us-east-2',
                        'us-west-1', 'us-west-2',
                        'eu-central-1',
-                       'eu-west-1', 'eu-west-2',
+                       'eu-west-1', 'eu-west-2', 'eu-west-3',
                        'ap-northeast-1', 'ap-northeast-2',
                        'ap-southeast-1', 'ap-southeast-2',
                        'ap-south-1',
