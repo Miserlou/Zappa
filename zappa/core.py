957327c19f61910a4f90f335ae94380ee4c3d947
Benoit Brayer <brayer.benoit@gmail.com>
2018-06-18 12:06:53 +0200

    Forget my two previous commits...
     IdentitySource is required, my bad...

--- zappa/core.py
+++ zappa/core.py
@@ -1362,8 +1362,7 @@ class Zappa(object):
         authorizer_resource.Name = authorizer.get("name", "ZappaAuthorizer")
         authorizer_resource.Type = authorizer_type
         authorizer_resource.AuthorizerUri = uri
-        if authorizer_type != 'REQUEST':  # Identity source isn't mandatory for request authorizers
-          authorizer_resource.IdentitySource = "method.request.header.%s" % authorizer.get('token_header', 'Authorization')
+        authorizer_resource.IdentitySource = "method.request.header.%s" % authorizer.get('token_header', 'Authorization')
           
         if identity_validation_expression:
             authorizer_resource.IdentityValidationExpression = identity_validation_expression
