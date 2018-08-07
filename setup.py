44b4e846667e8344e52297327d2b90c3f039e946
Ernesto Serrano <info@ernesto.es>
2018-08-07 10:27:15 +0200

    Support python 3.7
    Added an option to install Zappa in python 3.7 because is the official python3 installed via homebrew in MacOS

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
