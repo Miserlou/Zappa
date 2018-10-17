788b84be8fb3aab2d6f2255aae9c4131084e787c
João Miguel Neves <joao@silvaneves.org>
2018-10-17 22:27:15 +0100

    fix wrong call

--- zappa/core.py
+++ zappa/core.py
@@ -2826,7 +2826,7 @@ class Zappa(object):
         Get the Hosted Zone ID for a given domain.
 
         """
-        all_zones = self.route53.get_all_zones()
+        all_zones = self.get_all_zones()
         return self.get_best_match_zone(all_zones, domain)
 
     @staticmethod
