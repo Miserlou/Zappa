3a97fb51bf6e14b843cf2d0ee10f87ff3fb8a1f0
Ilya Sukhanov <ilya@sukhanov.net>
2018-12-02 09:34:11 -0700

    Enable Private API GW via EndpointConfiguration
    This change makes EndpointConfiguration configurable this, primarily,
    enables for creation of Private API Gateway Endpoints, ones only
    accessible from within a VPC.
    
    It is an attempt to address:
    https://github.com/Miserlou/Zappa/issues/1540
    
    See also:
    https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-private-apis.html

--- zappa/cli.py
+++ zappa/cli.py
@@ -663,7 +663,8 @@ class ZappaCLI(object):
                                             authorizer=self.authorizer,
                                             cors_options=self.cors,
                                             description=self.apigateway_description,
-                                            policy=self.apigateway_policy
+                                            policy=self.apigateway_policy,
+                                            endpoint_configuration=self.endpoint_configuration
                                         )
 
         if not output:
@@ -801,7 +802,8 @@ class ZappaCLI(object):
                                                         iam_authorization=self.iam_authorization,
                                                         authorizer=self.authorizer,
                                                         cors_options=self.cors,
-                                                        description=self.apigateway_description
+                                                        description=self.apigateway_description,
+                                                        endpoint_configuration=self.endpoint_configuration
                                                     )
 
             self.zappa.update_stack(
@@ -981,7 +983,8 @@ class ZappaCLI(object):
                                             iam_authorization=self.iam_authorization,
                                             authorizer=self.authorizer,
                                             cors_options=self.cors,
-                                            description=self.apigateway_description
+                                            description=self.apigateway_description,
+                                            endpoint_configuration=self.endpoint_configuration
                                         )
             self.zappa.update_stack(
                                     self.lambda_name,
@@ -2076,6 +2079,7 @@ class ZappaCLI(object):
         self.binary_support = self.stage_config.get('binary_support', True)
         self.api_key_required = self.stage_config.get('api_key_required', False)
         self.api_key = self.stage_config.get('api_key')
+        self.endpoint_configuration = self.stage_config.get('endpoint_configuration', None)
         self.iam_authorization = self.stage_config.get('iam_authorization', False)
         self.cors = self.stage_config.get("cors", False)
         self.lambda_description = self.stage_config.get('lambda_description', "Zappa Deployment")
