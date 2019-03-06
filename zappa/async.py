077b533aa8155f1f3ed41946a3b917c7f7824eec
Ippei Ukai <ippei_ukai@mac.com>
2019-03-06 15:13:05 +0900

    Add backward compatibility module for now renamed zappa.async.
    re https://github.com/Miserlou/Zappa/pull/1762

---
+++ zappa/async.py
@@ -0,0 +1,5 @@
+# -*- coding: utf8 -*-
+import warnings
+
+warnings.warn('Module "zappa.async" is deprecated; please use "zappa.asynchronous" instead.', category=DeprecationWarning)
+from .asynchronous import *
