93fd2bc0b042396a6ac3b47ddd464c92c2e2d4b7
Kimbrian <kimbriancanavan@gmail.com>
2020-02-06 10:35:11 +1100

    Set python 3.8 manylinux wheel suffix correctly

--- tests/tests.py
+++ tests/tests.py
@@ -176,11 +176,11 @@ class TestZappa(unittest.TestCase):
 
     def test_get_manylinux_python38(self):
         z = Zappa(runtime='python3.8')
-        self.assertIsNotNone(z.get_cached_manylinux_wheel('psycopg2', '2.7.6'))
+        self.assertIsNotNone(z.get_cached_manylinux_wheel('psycopg2-binary', '2.8.4'))
         self.assertIsNone(z.get_cached_manylinux_wheel('derp_no_such_thing', '0.0'))
 
         # mock with a known manylinux wheel package so that code for downloading them gets invoked
-        mock_installed_packages = {'psycopg2': '2.7.6'}
+        mock_installed_packages = {'psycopg2-binary': '2.8.4'}
         with mock.patch('zappa.core.Zappa.get_installed_packages', return_value=mock_installed_packages):
             z = Zappa(runtime='python3.8')
             path = z.create_lambda_zip(handler_file=os.path.realpath(__file__))
--- zappa/core.py
+++ zappa/core.py
@@ -286,8 +286,12 @@ class Zappa(object):
             self.manylinux_wheel_file_suffix = 'cp27mu-manylinux1_x86_64.whl'
         elif self.runtime == 'python3.6':
             self.manylinux_wheel_file_suffix = 'cp36m-manylinux1_x86_64.whl'
-        else:
+        elif self.runtime == 'python3.7':
             self.manylinux_wheel_file_suffix = 'cp37m-manylinux1_x86_64.whl'
+        else:
+            # The 'm' has been dropped in python 3.8+ since builds with and without pymalloc are ABI compatible
+            # See https://github.com/pypa/manylinux for a more detailed explanation
+            self.manylinux_wheel_file_suffix = 'cp38-manylinux1_x86_64.whl'
 
         self.endpoint_urls = endpoint_urls
         self.xray_tracing = xray_tracing
