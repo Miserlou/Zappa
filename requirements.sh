5a4488fe5ad25d26eb60a8b1628b92fbf5588ff7
João Miguel Neves <github@silvaneves.org>
2020-02-21 20:06:43 +0000

    add update possibility to script and new requirements

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-pip-compile -o test_requirements.txt requirements.in test_requirements.in
+pip-compile $* -o test_requirements.txt requirements.in test_requirements.in
 cp test_requirements.txt requirements.txt
 pip-compile -o requirements.txt requirements.in
