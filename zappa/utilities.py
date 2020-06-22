23df4378d8530c17b033563d39f6d8e44f238063
Jerry Gregoire <jerry@arcadecoffee.com>
2020-06-22 17:40:43 -0500

    working for multiple stages

--- zappa/utilities.py
+++ zappa/utilities.py
@@ -210,6 +210,9 @@ def get_event_source(event_source, lambda_arn, target_function, boto_session, dr
     to schedule this event, and return the event source.
 
     """
+
+    # TODO: Jerry - fix for S3
+
     import kappa.function
     import kappa.restapi
     import kappa.event_source.base
@@ -351,10 +354,84 @@ def get_event_source(event_source, lambda_arn, target_function, boto_session, dr
             if self.filters:
                 self.add_filters(function)
 
+    # reimplementing this entire class rather than extending the Kappa version because only the constructor is reused
+    # and I didn't want any future usages or extentions to accidentally fall through to Kappa's object via one of the
+    # 'aliases' it sets up
+    class ImprovedS3EventSource(kappa.event_source.base.EventSource):
+        def __init__(self, context, config):
+            super(ImprovedS3EventSource, self).__init__(context, config)
+            self._s3 = kappa.awsclient.create_client('s3', context.session)
+
+        def _get_bucket_name(self):
+            return self.arn.split(':')[-1]
+
+        # this method is overridden / implemented below but I wanted the placeholder here for clarity and to make IDEs
+        # less cranky
+        def _make_notification_id(self, function_name):
+            raise NotImplementedError
+
+        def add(self, function):
+            event_config = {
+                'Id': self._make_notification_id(function.name),
+                'Events': [e for e in self._config['events']],
+                'LambdaFunctionArn': '%s:%s' % (function.arn, function._context.environment),
+            }
+
+            # Add S3 key filters
+            if 'key_filters' in self._config:
+                filters_spec = {'Key': {'FilterRules': []}}
+                for key_filter in self._config['key_filters']:
+                    if 'type' in key_filter and 'value' in key_filter and key_filter['type'] in ('prefix', 'suffix'):
+                        rule = {'Name': key_filter['type'], 'Value': key_filter['value']}
+                        filters_spec['Key']['FilterRules'].append(rule)
+                event_config['Filter'] = filters_spec
+
+            try:
+                bucket_config = self._s3.call('get_bucket_notification_configuration', Bucket=self._get_bucket_name())
+                del bucket_config['ResponseMetadata']
+                if 'LambdaFunctionConfigurations' in bucket_config:
+                    bucket_config['LambdaFunctionConfigurations'].append(event_config)
+                else:
+                    bucket_config['LambdaFunctionConfigurations'] = [event_config]
+                response = self._s3.call(
+                    'put_bucket_notification_configuration',
+                    Bucket=self._get_bucket_name(),
+                    NotificationConfiguration=bucket_config)
+                LOG.debug(response)
+            except Exception as exc:
+                LOG.debug(exc.response)
+                LOG.exception('Unable to add S3 event source')
+
+        def remove(self, function):
+            LOG.debug('removing s3 notification')
+            bucket_config = self._s3.call('get_bucket_notification_configuration', Bucket=self._get_bucket_name())
+            LOG.debug(bucket_config)
+
+            new_config = []
+            if 'LambdaFunctionConfigurations' in bucket_config:
+                for configuration in bucket_config['LambdaFunctionConfigurations']:
+                    if configuration['Id'] != self._make_notification_id(function.name):
+                        new_config.append(configuration)
+            response = self._s3.call('put_bucket_notification_configuration', Bucket=self._get_bucket_name(),
+                                     NotificationConfiguration={'LambdaFunctionConfigurations': new_config})
+            LOG.debug(response)
+
+        def status(self, function):
+            LOG.debug('status for s3 notification for %s', function.name)
+            bucket_config = self._s3.call('get_bucket_notification_configuration', Bucket=self._get_bucket_name())
+            LOG.debug(bucket_config)
+            if 'LambdaFunctionConfigurations' in bucket_config:
+                for configuration in bucket_config['LambdaFunctionConfigurations']:
+                    if configuration['Id'] == self._make_notification_id(function.name):
+                        return {
+                            'LambdaFunctionConfiguration': configuration,
+                            'State': 'Enabled'
+                        }
+
     event_source_map = {
         'dynamodb': kappa.event_source.dynamodb_stream.DynamoDBStreamEventSource,
         'kinesis': kappa.event_source.kinesis.KinesisEventSource,
-        's3': kappa.event_source.s3.S3EventSource,
+        's3': ImprovedS3EventSource,
         'sns': ExtendedSnsEventSource,
         'sqs': SqsEventSource,
         'events': kappa.event_source.cloudwatch.CloudWatchEventSource
@@ -407,6 +484,8 @@ def add_event_source(event_source, lambda_arn, target_function, boto_session, dr
     Given an event_source dictionary, create the object and add the event source.
     """
 
+    # TODO: Jerry - fix for S3 - need .status and .add
+
     event_source_obj, ctx, funk = get_event_source(event_source, lambda_arn, target_function, boto_session, dry=False)
     # TODO: Detect changes in config and refine exists algorithm
     if not dry:
@@ -423,6 +502,8 @@ def remove_event_source(event_source, lambda_arn, target_function, boto_session,
     Given an event_source dictionary, create the object and remove the event source.
     """
 
+    # TODO: Jerry - fix for S3 - need .status and .remove
+
     event_source_obj, ctx, funk = get_event_source(event_source, lambda_arn, target_function, boto_session, dry=False)
 
     # This is slightly dirty, but necessary for using Kappa this way.
@@ -437,6 +518,8 @@ def get_event_source_status(event_source, lambda_arn, target_function, boto_sess
     """
     Given an event_source dictionary, create the object and get the event source status.
     """
+
+    # TODO: Jerry - fix for S3 - need .status  (actually, this method appears unused...)
 
     event_source_obj, ctx, funk = get_event_source(event_source, lambda_arn, target_function, boto_session, dry=False)
     return event_source_obj.status(funk)
