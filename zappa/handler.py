1e7d9c18ecbcf244219d14ddb80e9a690079f52e
piotr.drazikowski <piotr.drazikowski@jmr.pl>
2021-01-11 15:18:30 +0100

    handling sns messages that are parsed by json.loads() but are other than a dict

--- zappa/handler.py
+++ zappa/handler.py
@@ -301,7 +301,7 @@ class LambdaHandler:
                 message = json.loads(record['Sns']['Message'])
                 if message.get('command'):
                     return message['command']
-            except ValueError:
+            except (AttributeError, ValueError):
                 pass
             arn = record['Sns'].get('TopicArn')
         elif 'dynamodb' in record or 'kinesis' in record:
