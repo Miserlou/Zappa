979278700c6e1903b116a41490c9f0ccc3a256e9
Sha <wangsha@users.noreply.github.com>
2021-03-16 14:26:14 +0800

    Update core.py
    point zappa to proviate GitHub repo to fix package dependency conflict.
    command to reproduce: 
    `pip install --quiet --target /Users/aloha/Projects/enterprise/handler_venv/lib/python3.7/site-packages cfn-flip==1.2.3 importlib-metadata==3.7.0 typing-extensions==3.7.4.3 requests==2.25.1 Werkzeug==1.0.1 argcomplete==1.12.2 kappa==0.6.0 python-dateutil==2.8.1 troposphere==2.6.3 pip==21.0.1 future==0.18.2 wsgi-request-logger==0.4.6 tqdm==4.58.0 boto3==1.17.17 durationpy==0.5 placebo==0.9.0 text-unidecode==1.3 six==1.15.0 s3transfer==0.3.4 botocore==1.20.17 urllib3==1.26.3 chardet==3.0.4 hjson==3.0.2 pip-tools==5.5.0 zipp==3.4.0 toml==0.10.2 python-slugify==4.0.1 certifi==2020.12.5 jmespath==0.10.0 idna==2.10 PyYAML==5.4.1 click==7.1.2 wheel==0.36.2 setuptools zappa=0.52.0

--- zappa/core.py
+++ zappa/core.py
@@ -422,7 +422,10 @@ class Zappa:
 
         # Use pip to download zappa's dependencies. Copying from current venv causes issues with things like PyYAML that installs as yaml
         zappa_deps = self.get_deps_list('zappa')
-        pkg_list = ['{0!s}=={1!s}'.format(dep, version) for dep, version in zappa_deps]
+        pkg_list = ['{0!s}=={1!s}'.format(dep, version) for dep, version in zappa_deps if dep != 'zappa']
+        
+        # add private zappa dependency
+        pkg_list.append("git+https://github.com/wangsha/Zappa.git")
 
         # Need to manually add setuptools
         pkg_list.append('setuptools')
