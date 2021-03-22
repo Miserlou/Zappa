f4723a4fadf092cd963907d95c401aaae8494681
jiho lee <optional.int@kakao.com>
2021-03-23 07:20:58 +0900

    Fix: cli zappa github url
    adding makefile
    
    limiting flake to zappa
    
    travis uses make commands for consistency
    
    using job stages for travis
    
    use v2 LE endpoint
    
    moving black check to other PR
    
    install pip-tools
    
    missing a quote mark
    
    going back to single script key

--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,7 @@ if [ "$1" == "--upgrade" ]; then
     ARGS="-U"
 fi
 
+pip install -U pip-tools
 pip-compile ${ARGS} -o test_requirements.txt requirements.in test_requirements.in
 cp test_requirements.txt requirements.txt
 pip-compile -o requirements.txt requirements.in
