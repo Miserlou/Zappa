ddc3fbb5892a6b9dd42209de58d0bd6d6b34fb7b
John (Ottenlips) Franke <jottenlips@gmail.com>
2021-12-29 00:15:46 -0600

    remove library that aws lambda cannot find (#1060)
    * remove library that aws lambda cannot find
    
    * run black
    
    Co-authored-by: javulticat <31746850+javulticat@users.noreply.github.com>

--- zappa/handler.py
+++ zappa/handler.py
@@ -101,9 +101,7 @@ class LambdaHandler:
             # https://github.com/Miserlou/Zappa/issues/776
             is_slim_handler = getattr(self.settings, "SLIM_HANDLER", False)
             if is_slim_handler:
-                included_libraries = getattr(
-                    self.settings, "INCLUDE", ["libmysqlclient.so.18"]
-                )
+                included_libraries = getattr(self.settings, "INCLUDE", [])
                 try:
                     from ctypes import cdll, util
 
